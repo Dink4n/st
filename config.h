a8639a5e5a25991544a4b0fd36e85808b08b187e
JimboKearn <anujithbahuleyan@gmail.com>
2021-01-28 21:31:14 +0530

    black background fixed

--- config.h
+++ config.h
@@ -137,7 +137,7 @@ static const char *colorname[] = {
  * foreground, background, cursor
  */
 unsigned int defaultfg = 15;
-unsigned int defaultbg = 258;
+unsigned int defaultbg = 0;
 static unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
