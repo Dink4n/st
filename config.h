b7623888a171915128dd216ee5b6616b9b88b47b
JimboKearn <anujithbahuleyan@gmail.com>
2021-02-02 01:59:44 +0530

    font perfecto

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Mono:size=14:antialias=true:autohint=true";
 static int borderpx = 20;
 
 /*
